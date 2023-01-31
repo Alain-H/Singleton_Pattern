1aad166a9ed5187dfaa90c38c60d261f0b07e3b9
Alain Hoch <alain.hoch@hotmail.com>
2023-01-31 21:31:22 +0100

    push total code + Doku

---
+++ Singleton_Pattern/Ork_General.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    internal class Ork_General
+    {
+
+        public string name;
+        private static Ork_General My_General;   
+        private Ork_General()
+        {
+
+        }
+        public static Ork_General getInstance()
+        {
+            if (My_General == null)
+            {
+                My_General = new Ork_General();
+            }
+            return My_General;
+        }
+
+        public void setName(string General)
+        {
+            name = General;
+        }
+        public string AngrifsBefehl()
+        {
+            return name + " Befehl: Jetzt greifen wir an!";
+        }
+        public string Rueckzugsbefehl()
+        {
+            return name + " Befehl: Wir ziehen uns zurück!"!;
+        }
+    }
+}
---
+++ Singleton_Pattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using Singleton_Pattern;
+
+Ork_General King = Ork_General.getInstance();
+King.setName("Ludwig-XIV");
+Console.WriteLine(King.AngrifsBefehl());
+
+
+Ork_General King2 = Ork_General.getInstance();
+King2.setName("Max XIII");
+Console.WriteLine(King2.Rueckzugsbefehl());
+Console.WriteLine(King.AngrifsBefehl());  //zum veranschaulichen dass King nicht mehr existiert in der Konsole kommt wieder King2 mit dem Angriffsbefehl
+                                           // da die Instanz von King beendet wurde.
+
+Console.ReadKey();
